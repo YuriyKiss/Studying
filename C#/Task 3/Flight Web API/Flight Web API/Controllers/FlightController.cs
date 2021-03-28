229401743bc99a63754601fa09681725ccf1e6a5
Morgot <yur.cis3@gmail.com>
2021-03-29 01:56:29 +0300

    Task 3 Done
    next: task 4 (swagger docs + unit tests)

--- C#/Task 3/Flight Web API/Flight Web API/Controllers/FlightController.cs
+++ C#/Task 3/Flight Web API/Flight Web API/Controllers/FlightController.cs
@@ -17,11 +17,20 @@ namespace Flight_Web_API.Controllers
         }
 
         [HttpGet]
+        public ActionResult GetAll(string search, string sortBy, string sortOrder, int offset, int limit)
+        {
+            var (result, count) = service.GetAll(search, sortBy, sortOrder, offset, limit);
+            if (count > 0)
+                return Ok(new { result, count });
+            return BadRequest("No orders found");
+        }
+
+        [HttpGet]
         [Route("{id}")]
         public ActionResult GetOne(int id)
         {
             Flight get = service.GetOne(id);
-            if (!(get == null))
+            if (get != null)
                 return Ok(get);
             return NotFound("Flight with such ID was not found");
         }
@@ -33,7 +42,7 @@ namespace Flight_Web_API.Controllers
             {
                 if(service.Create(toAdd) == 201)
                     return Created($"api/flights/{toAdd.ID}", "Flight was created successfully");
-                return BadRequest("Flight ID already exist, or validation didn't pass");
+                return BadRequest("Flight ID already exist");
             }
             return BadRequest("POST get incorrect data values");
         }
@@ -56,7 +65,7 @@ namespace Flight_Web_API.Controllers
             {
                 if(service.Edit(id, toEdit) == 202)
                     return Accepted($"api/flights/{toEdit.ID}", "Flight was edited successfully");
-                return BadRequest("Flight ID does not exist, or validation didn't pass");
+                return NotFound("Flight ID does not exist");
             }
             return BadRequest("PUT get incorrect data values");
         }
--- C#/Task 3/Flight Web API/Flight Web API/Services/FlightControllerService.cs
+++ C#/Task 3/Flight Web API/Flight Web API/Services/FlightControllerService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Reflection;
 using System.Collections.Generic;
 using Flight_Web_API.Models;
 
@@ -16,8 +17,45 @@ namespace Flight_Web_API.Sevices
 
         public (List<Flight> result, int count) GetAll(string search, string sortBy, string sortOrder, int offset, int limit)
         {
-            throw new NotImplementedException();
+            List<Flight> data = context.Flights.ToList<Flight>();
+
+            if (search != null)
+                foreach (Flight flight in data.ToList())
+                    foreach (PropertyInfo prop in typeof(Flight).GetProperties())
+                    {
+                        if (prop.GetValue(flight).ToString().ToLower().Contains(search.ToLower()))
+                            break;
+                        if (prop.Name == typeof(Flight).GetProperties().Last().Name) data.Remove(flight);
+                    }
+
+            foreach (PropertyInfo prop in typeof(Flight).GetProperties())
+            {
+                if (prop.Name == sortBy) break;
+                if (prop.Name == typeof(Flight).GetProperties().Last().Name) sortBy = null;
+            }
+
+            if (sortBy != null)
+            {
+                if(sortOrder == "desc")
+                    data = data.OrderByDescending(o => typeof(Flight).GetProperty(sortBy).GetValue(o)).ToList();
+                else
+                    data = data.OrderBy(o => typeof(Flight).GetProperty(sortBy).GetValue(o)).ToList();
+            }
+            else 
+            {
+                if (sortOrder == "desc")
+                    data = data.OrderByDescending(o => o.ID).ToList();
+                else
+                    data = data.OrderBy(o => o.ID).ToList();
+            }
+
+            if (limit == null || limit < 1) limit = data.Count;
+            if (offset == null || offset < 1) offset = 1;
+            data = data.Skip(limit * (offset - 1)).Take(limit).ToList();
+
+            return (data, data.Count);
         }
+
         public Flight GetOne(int id)
         {
             return context.Flights.Find(id);
@@ -33,6 +71,7 @@ namespace Flight_Web_API.Sevices
             catch { return 404; }
             return 201;
         }
+
         public int Delete(int id)
         {
             Flight to_remove = null;
@@ -47,14 +86,20 @@ namespace Flight_Web_API.Sevices
             }
             return 404;
         }
+
         public int Edit(int id, Flight toEdit)
         {
             var entity = context.Flights.FirstOrDefault(item => item.ID == id);
 
             if (entity != null)
             {
-                context.Flights.Remove(entity);
-                context.Flights.Add(toEdit);
+                foreach (var prop in toEdit.GetType().GetProperties())
+                {
+                    try { prop.SetValue(entity, prop.GetValue(toEdit)); }
+                    catch { return 401; }
+                }
+
+                context.Flights.Update(entity);
                 context.SaveChanges();
                 return 202;
             }
